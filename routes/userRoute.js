12e211be3c4f787e639c8488dde9d26d3623327a
Arjun Jaishi <a.jaishi7777777@gmail.com>
2023-06-11 19:20:55 +0545

    Update User | findByIdAndUpdate | findOneAndUpdate

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -4,7 +4,7 @@ const User = require("../model/User");
 
 /**
  * Create User Route
- * GET: http://locahost:3000/user
+ * GET : http://locahost:3000/user
  */
 router.post("/user", async function (req, res) {
   try {
@@ -21,7 +21,7 @@ router.post("/user", async function (req, res) {
 
 /**
  * Fetch Single User By Params
- * GET: http://localhost:3000/user/:id
+ * GET : http://localhost:3000/user/:id
  *
  */
 
@@ -38,7 +38,7 @@ router.get("/user/:id", async function (req, res) {
 
 /**
  * Find all users
- * GET: http://localhost:3000/user/
+ * GET : http://localhost:3000/user/
  *
  */
 
@@ -53,7 +53,7 @@ router.get("/user", async function (req, res) {
 /**
  *
  * Find user by query
- * GET http://localhost:3000/users?id=6485ba227aa9ace5003b899d
+ * GET : http://localhost:3000/users?id=6485ba227aa9ace5003b899d
  *
  *
  */
@@ -69,13 +69,11 @@ router.get("/users", async function (req, res) {
 
 /**
  * Delete User
- * http://locahost:3000/user/:id
+ * DELETE : http://locahost:3000/user/:id
  *
  */
 
 router.delete("/user/:id", async function (req, res) {
-  const user = User.findById({ _id: req.params.id });
-
   const data = await User.deleteOne({ _id: req.params.id });
   if (data) {
     if (data.deletedCount == 0) {
@@ -89,6 +87,27 @@ router.delete("/user/:id", async function (req, res) {
     });
   }
   return res.json({ msg: "Unable to delete user" });
+});
+
+/**
+ * Update User
+ * PUT : http://localhost:3000/user/id
+ */
+
+router.put("/user/:id", async function (req, res) {
+  const user = await User.findOne({ _id: req.params.id });
+  if (!user) {
+    return res.json({ msg: "No User Found" });
+  }
+  const updateUser = await User.findOneAndUpdate(
+    { _id: req.params.id },
+    req.body,
+    { new: true }
+  );
+  if (updateUser) {
+    return res.json({ msg: "User Updated", updateUser });
+  }
+  res.json({ msg: "JPT" });
 });
 
 module.exports = router;
